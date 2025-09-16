c5b84bbc908b2887ea8a09adcbcbc3e8cc35784d
mithalikp25 <mithali.p@s.amity.edu>
2025-09-16 16:43:37 +0530

    pushing more java pattern problems

---
+++ pattern1.java
@@ -0,0 +1,19 @@
+class pattern1{
+    public static void main(String[] args){
+        for(int rows=0;rows<=6;rows++){
+            for(int cols=0;cols<=6;cols++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//output
+// *******
+// *******
+// *******
+// *******
+// *******
+// *******
+// *******
