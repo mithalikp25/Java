6e4a0cfba3a4b6a15257f1b9f95799c2a3c0e626
mithalikp25 <mithali.p@s.amity.edu>
2025-09-16 13:08:19 +0530

    pushing more Java files

---
+++ conditions.java
@@ -0,0 +1,28 @@
+//if/else-if/else 
+
+public class conditions {
+    public static void main(String[] args) {
+        int marks = 72;
+
+        if (marks >= 90) {
+            System.out.println("Grade: A"); // For marks 90 and above
+        } else if (marks >= 75) {
+            System.out.println("Grade: B"); // For marks 75 to 89
+        } else if (marks >= 50) {
+            System.out.println("Grade: C"); // For marks 50 to 74
+        } else {
+            System.out.println("Fail");     // For marks below 50
+        }
+    }
+}
+
+
+//ternary operators
+
+public class conditions {
+    public static void main(String[] args) {
+        int marks = 72;
+        String result=(marks>=70)?"distinction":"better luck next time";
+        System.out.println(result);
+    }
+}
