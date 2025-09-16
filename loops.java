d7cbddeea5600d8bdf9d98a3163768b1f1bc2ef4
mithalikp25 <mithali.p@s.amity.edu>
2025-09-16 12:22:08 +0530

    Initial commit - pushing more Java files

---
+++ loops.java
@@ -0,0 +1,22 @@
+//for loop
+ 
+class loops{
+    public static void main(String[] args){
+        for(int i=0;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
+
+
+//while loop
+
+class loops{
+    public static void main(String[] args){
+        int j=1;
+        while(j<=10){
+            System.out.println(j);
+            j++;
+        }
+    }
+}
