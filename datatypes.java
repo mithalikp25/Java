73f0f7a280702287cfcd51469c5c31a5284e08f2
mithalikp25 <mithali.p@s.amity.edu>
2025-09-16 11:36:50 +0530

    Initial commit - pushing Java files

---
+++ datatypes.java
@@ -0,0 +1,29 @@
+class Datatypes{
+    public static void main(String[] args){
+        // integer
+        // char
+        // Stringfloat
+        // double
+        // boolean
+
+        //datatype name=data;
+
+        int age=16;
+        System.out.println(age);
+
+        String name="mithali";
+        System.out.println(name);
+
+        float digit=6.400254f;
+        System.out.println(digit);
+
+        double doub=6.4002;
+        System.out.println(doub);
+
+        boolean decison=true;
+        System.out.println(decison);
+
+        char newo='a';
+        System.out.println(newo);
+    }
+}
