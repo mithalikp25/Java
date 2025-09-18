c0cbbeaff9d9ac8ce3bfa7da18b5d86902994d27
mithalikp25 <mithali.p@s.amity.edu>
2025-09-18 23:47:52 +0530

    pushing more java pattern problems

--- pattern13.java
+++ pattern13.java
@@ -25,3 +25,33 @@ class pattern13 {
 // ***   ***
 // **     **
 // *       *
+
+import java.util.Scanner;
+
+class pattern13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the rows: ");
+        int n = sc.nextInt();
+
+        for (int r = 1; r <= n; r++) {   // increasing loop, no rows--
+            for (int c = 1; c <= 2*n-1; c++) {
+                // 🔄 modified condition: stars shrink outward
+                if (c <= r || c >= (2*n - r)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// enter the rows: 5
+// *       *
+// **     **
+// ***   ***
+// **** ****
+// *********
