8692f170696b44c9e51ecb39bd5fa887ab315e20
mithalikp25 <mithali.p@s.amity.edu>
2025-09-26 10:33:01 +0530

    added my first class and object problem

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+class Calculator{
+    public int add(int n1,int n2){
+        int r=n1+n2;
+        return r;
+    }
+}
+
+
 class Main{
-    public static void main(String[]args){
-        System.out.print("hello");
+    public static void main(String[] args){
+        Calculator calc=new Calculator();
+        //int result= calc.add(3,5);
+        System.out.print(calc.add(3,6));
     }
 }
