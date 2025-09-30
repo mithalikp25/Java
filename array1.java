2e0045b7a3d9048c641ec5206c6a7d030888647c
mithalikp25 <mithali.p@s.amity.edu>
2025-09-30 12:36:21 +0530

    pushing array

---
+++ array1.java
@@ -0,0 +1,11 @@
+class array1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(arr[0]);
+
+        int nums[]= new int[5];
+        System.out.println(nums[0]);//since all the elements are initialized to 0 by default
+        nums[0]=10;//assigning value to first index
+        System.out.println(nums[0]);
+    }
+}
--- arrays/array1.java
+++
@@ -1,5 +0,0 @@
-class array1 {
-    public static void main(String[] args) {
-        
-    }
-}
