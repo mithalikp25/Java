c1e18167d3f1a5447a5ce0e464047f40ca18693c
mithalikp25 <mithali.p@s.amity.edu>
2025-11-06 14:18:01 +0530

    adding number based logic builder problems

---
+++ logic_building/num_based5.java
@@ -0,0 +1,49 @@
+//Print all prime numbers between 1 and 100. 
+
+package logic_building;
+
+import java.util.*;
+
+public class num_based5 {
+    public static void main(String[] args) {
+
+        for(int i=1;i<100;i++){
+            int count=0;
+            for(int j=1;j<=i;j++){
+                if(i%j==0){
+                    count++;
+                }
+            }
+            if(count==2){
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+//output:
+// 2
+// 3
+// 5
+// 7
+// 11
+// 13
+// 17
+// 19
+// 23
+// 29
+// 31
+// 37
+// 41
+// 43
+// 47
+// 53
+// 59
+// 61
+// 67
+// 71
+// 73
+// 79
+// 83
+// 89
+// 97
