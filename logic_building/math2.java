0079e5ae989e073e641f32334cf757c68ac90654
mithalikp25 <mithali.p@s.amity.edu>
2025-11-17 10:02:18 +0530

    pushing cube logic builder problem

---
+++ logic_building/math2.java
@@ -0,0 +1,28 @@
+package logic_building;
+
+import java.util.*;
+
+public class math2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter a number:");
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            System.out.println((int) Math.pow(i, 3));
+        }
+    }
+}
+
+// Output:
+// enter a number:10
+// 1
+// 8
+// 27
+// 64
+// 125
+// 216
+// 343
+// 512
+// 729
+// 1000
