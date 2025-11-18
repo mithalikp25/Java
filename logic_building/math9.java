595e9d3b8b336779563d745373264cd730bcf42f
mithalikp25 <mithali.p@s.amity.edu>
2025-11-19 05:19:46 +0530

    pushing highest common factors logic builder

--- logic_building/math9.java
+++ logic_building/math9.java
@@ -11,25 +11,30 @@ import java.util.*;
 
 public class math9 {
     public static void main(String[] args) {
-        {Scanner sc=new Scanner(System.in);
-    System.out.print("enter first number:");
-    int n=sc.nextInt();
-    System.out.print("enter second number:");
-    int m=sc.nextInt();
 
-    int hcf=0;
-    for(int i=n; i>=1; i--){  
-        if(n%i==0 && m%i==0){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter first number:");
+        int n=sc.nextInt();
+        System.out.print("enter second number:");
+        int m=sc.nextInt();
+
+        int hcf=0;
+        for(int i=n; i>=1; i--){  
+            if(n%i==0 && m%i==0){
             hcf=i;
             break;  // Exit loop as soon as HCF is found
             }
       }
     System.out.print("HCF is:"+hcf);}
     }
-}
+
 
 // Output:
 
 // enter first number:75
 // enter second number:90
 // HCF is:15
+
+// enter first number:100
+// enter second number:50
+// HCF is:50
