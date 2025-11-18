009386e7bed0990097aa4a240b9d4e426d86b520
mithalikp25 <mithali.p@s.amity.edu>
2025-11-19 05:07:22 +0530

    pushing common factors logic builder

---
+++ hacker_rank/hacker37.java
@@ -0,0 +1,9 @@
+package hacker_rank;
+
+import java.util.*;
+
+public class hacker37 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ logic_building/math7.java
@@ -0,0 +1,28 @@
+//get common factors between two numbers
+
+package logic_building;
+
+import java.util.*;
+
+public class math7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter first number:");
+        int n=sc.nextInt();
+        System.out.print("enter second number:");
+        int m=sc.nextInt();
+
+        for(int i=1;i<n;i++){
+            if(n%i==0 && m%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
+
+
+// output:  
+
+// enter first number:75
+// enter second number:90
+// 1 3 5 15 
