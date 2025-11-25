6a41328456808057d2f0e2f9bad50a97e26a7e1e
mithalikp25 <mithali.p@s.amity.edu>
2025-11-25 12:20:56 +0530

    pushing fizzbuzz program

--- logic_building/fizzBizz.java
+++ logic_building/fizzBizz.java
@@ -10,19 +10,28 @@ public class fizzBizz {
         System.out.println("enter a number");
         int a=sc.nextInt();
 
-        if(a%3==0){
-            System.out.println("fizz");
+        if((a%3==0)&&(a%5==0)){
+            System.out.println("fizzbuzz");
         }
         else if(a%5==0){
             System.out.println("buzz");
         }
-        else if((a%3==0)&&(a%5==0)){
-            System.out.println("fizzbuzz");
+        else if((a%3==0)){
+            System.out.println("fizz");
         }
       }
 }
 
 // Output:
+
+// enter a number
+// 3
+// fizz
+
 // enter a number
 // 5
 // buzz
+
+// enter a number
+// 15
+// fizzbuzz
