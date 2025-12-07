f0e77214da5d133300aaffd849623623a8dd4215
mithalikp25 <mithali.p@s.amity.edu>
2025-12-08 02:42:17 +0530

    pushing notes file

--- logic_building/notes.java
+++ logic_building/notes.java
@@ -6,7 +6,7 @@ public class notes {
   }
 }
 
-//Non-primitive data types are called reference types because they refer to objects.
+//---------> Non-primitive data types are called reference types because they refer to objects.
 
 // The main differences between primitive and non-primitive data types are:
 
@@ -14,3 +14,21 @@ public class notes {
 // Non-primitive types can be used to call methods to perform certain operations, whereas primitive types cannot.
 // Primitive types start with a lowercase letter (like int), while non-primitive types typically starts with an uppercase letter (like String).
 // Primitive types always hold a value, whereas non-primitive types can be null.
+
+
+
+//---------> The var keyword lets the compiler automatically detect the type of a variable based on the value you assign to it.
+
+// This helps you write cleaner code and avoid repeating types, especially for long or complex types.
+// For example, instead of writing int x = 5;, you can write:
+// var x = 5;  // x is an int
+// System.out.println(x);
+//var only works when you assign a value at the same time (you can't declare var x; without assigning a value)
+
+//for more complex types, such as ArrayList or HashMap, var can make the code shorter and easier to read:
+
+// Example
+// Without var
+// ArrayList<String> cars = new ArrayList<String>();
+// With var
+// var cars = new ArrayList<String>();
