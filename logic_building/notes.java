eed804c458d002a03d1905ae8b3b1534053cbbbe
mithalikp25 <mithali.p@s.amity.edu>
2025-12-08 02:33:16 +0530

    pushing notes file

---
+++ logic_building/notes.java
@@ -0,0 +1,16 @@
+package logic_building;
+
+public class notes {
+  public static void main(String[] args) {
+    System.out.println("Notes file for logic building");
+  }
+}
+
+//Non-primitive data types are called reference types because they refer to objects.
+
+// The main differences between primitive and non-primitive data types are:
+
+// Primitive types in Java are predefined and built into the language, while non-primitive types are created by the programmer (except for String).
+// Non-primitive types can be used to call methods to perform certain operations, whereas primitive types cannot.
+// Primitive types start with a lowercase letter (like int), while non-primitive types typically starts with an uppercase letter (like String).
+// Primitive types always hold a value, whereas non-primitive types can be null.
