161d1535a93e20819f884021d7e33592234648d9
mithalikp25 <mithali.p@s.amity.edu>
2025-12-09 12:24:50 +0530

    pushing looping syntax file

---
+++ AlgoForge/loop.java
@@ -0,0 +1,48 @@
+package AlgoForge;
+
+public class loop {
+  public static void main(String[] args) {
+
+        // ------------------------------
+        // 1. FOR LOOP
+        // ------------------------------
+        System.out.println("FOR LOOP:");
+        for (int i = 0; i <= 5; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        // ------------------------------
+        // 2. WHILE LOOP
+        // ------------------------------
+        System.out.println("WHILE LOOP:");
+        int j = 0;
+        while (j <= 5) {
+            System.out.print(j + " ");
+            j++; // increment inside loop
+        }
+        System.out.println("\n");
+
+        // ------------------------------
+        // 3. DO-WHILE LOOP
+        // ------------------------------
+        System.out.println("DO-WHILE LOOP:");
+        int k = 0;
+        do {
+            System.out.print(k + " ");
+            k++; // increment inside loop
+        } while (k <= 5);
+        System.out.println("\n");
+    }
+}
+
+// Output:
+
+// FOR LOOP:
+// 0 1 2 3 4 5 
+
+// WHILE LOOP:
+// 0 1 2 3 4 5 
+
+// DO-WHILE LOOP:
+// 0 1 2 3 4 5 
